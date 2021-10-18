cbe893eb787fd73761ee5684f292785c486e2800
Tymoteusz  Brzozowski <brzozowt@wit.edu.pl>
2021-10-18 21:11:56 +0200

    Scene changes, last scene

--- ScreamJam 2021/Assets/Scripts/PlayerInteract.cs
+++ ScreamJam 2021/Assets/Scripts/PlayerInteract.cs
@@ -29,7 +29,7 @@ public class PlayerInteract : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && targetObject != null)
         {
             targetObject?.Interact();
         }
